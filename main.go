16e4715394e8aac4e15226d5d314410e53baf6bb
osawa-koki <github@osawa.dev>
2023-01-19 01:01:26 +0900

    コード書いた。

--- main.go
+++ main.go
@@ -1,37 +1,43 @@
-// Package p contains an HTTP Cloud Function.
 package p
 
 import (
-	"encoding/json"
-	"fmt"
-	"html"
-	"io"
-	"log"
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
 	"net/http"
 )
 
-// HelloWorld prints the JSON encoded "message" field in the body
-// of the request or "Hello, World!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
 
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		switch err {
-		case io.EOF:
-			fmt.Fprint(w, "Hello World!")
-			return
-		default:
-			log.Printf("json.NewDecoder: %v", err)
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrot(z))
 		}
 	}
 
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
-		return
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
 	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	return color.Black
 }
